332443f983276e0dbc608d247b52677ab54bc198
Pavel Otchertsov <pavel.otchertsov@gmail.com>
2023-03-11 17:31:25 +0300

    shfmt hook

--- update-alternatives-gcc.sh
+++ update-alternatives-gcc.sh
@@ -4,36 +4,38 @@ set -e
 version="$1"
 priority="$2"
 
-for pkg in \
-    "gcc-${version}" \
-    "g++-${version}" \
-    "cpp-${version}" \
-    "gcc-${version}-arm-linux-gnueabihf" \
-    "g++-${version}-arm-linux-gnueabihf" \
-    "cpp-${version}-arm-linux-gnueabihf" \
-    "gcc-${version}-aarch64-linux-gnu" \
-    "g++-${version}-aarch64-linux-gnu" \
-    "cpp-${version}-aarch64-linux-gnu" \
-; do
-    if [ "$(dpkg-query -W -f='${Status}' "${pkg}" 2>/dev/null | grep -c "ok installed")" -eq 1 ]; then
-        dpkg-query -L "${pkg}" | grep "^/usr/bin/" | grep "\-${version}\$" | while read -r link; do
-        path="${link%-"${version}"}"
-        name="$(basename "${path}")"
-        update-alternatives --force --remove-all "${name}" 2>/dev/null | true
-        update-alternatives --force --install "${path}" "${name}" "${link}" "${priority}"
-        update-alternatives --auto "${name}"
-        done
-    fi
+pkgs=(
+  "gcc-${version}"
+  "g++-${version}"
+  "cpp-${version}"
+  "gcc-${version}-arm-linux-gnueabihf"
+  "g++-${version}-arm-linux-gnueabihf"
+  "cpp-${version}-arm-linux-gnueabihf"
+  "gcc-${version}-aarch64-linux-gnu"
+  "g++-${version}-aarch64-linux-gnu"
+  "cpp-${version}-aarch64-linux-gnu"
+)
+
+for pkg in "${pkgs[@]}"; do
+  if [ "$(dpkg-query -W -f='${Status}' "${pkg}" 2> /dev/null | grep -c "ok installed")" -eq 1 ]; then
+    dpkg-query -L "${pkg}" | grep "^/usr/bin/" | grep "\-${version}\$" | while read -r link; do
+      path="${link%-"${version}"}"
+      name="$(basename "${path}")"
+      update-alternatives --force --remove-all "${name}" 2> /dev/null | true
+      update-alternatives --force --install "${path}" "${name}" "${link}" "${priority}"
+      update-alternatives --auto "${name}"
+    done
+  fi
 done
 
-update-alternatives --force --remove-all cc 2>/dev/null | true
+update-alternatives --force --remove-all cc 2> /dev/null | true
 update-alternatives --force --install /usr/bin/cc cc /usr/bin/gcc "${priority}"
 update-alternatives --force --set cc /usr/bin/gcc
 
-update-alternatives --force --remove-all cxx 2>/dev/null | true
+update-alternatives --force --remove-all cxx 2> /dev/null | true
 update-alternatives --force --install /usr/bin/cxx cxx /usr/bin/g++ "${priority}"
 update-alternatives --force --set cxx /usr/bin/g++
 
-update-alternatives --force --remove-all c++ 2>/dev/null | true
+update-alternatives --force --remove-all c++ 2> /dev/null | true
 update-alternatives --force --install /usr/bin/c++ c++ /usr/bin/g++ "${priority}"
 update-alternatives --force --set c++ /usr/bin/g++
